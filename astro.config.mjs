fa463960fdb856b5b75d11cfd8a274d26d9ac42a
techne98 <techne98@proton.me>
2025-12-25 21:51:32 +0700

    Set code block theme as Catppuccin Macchiato

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+      shikiConfig: {
+          theme: "catppuccin-macchiato",
+      },
+  },
   experimental: {
     fonts: [
       {
