6bfe1ea32a02b46239541d166be8c208f1226cef
techne98 <techne98@proton.me>
2025-12-26 21:06:34 +0700

    Remove fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,14 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-      shikiConfig: {
-          theme: "catppuccin-macchiato",
-      },
-  },
-  experimental: {
-    fonts: [
-      {
-        provider: fontProviders.fontsource(),
-        name: "Monaspace Neon",
-        cssVariable: "--font-monaspace-neon",
-        weights: [400, 500, 700],
-        styles: ["normal", "italic"],
-      },
-    ],
+    shikiConfig: {
+      theme: "catppuccin-macchiato",
+    },
   },
   vite: {
     plugins: [tailwindcss()],
