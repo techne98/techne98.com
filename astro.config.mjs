40fead8f5b65d22d89ea8ff5fd9582984bfbac23
techne98 <techne98@proton.me>
2025-12-29 14:36:12 +0700

    Add and setup sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +13,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://techne98.com",
+  integrations: [sitemap()],
 });
